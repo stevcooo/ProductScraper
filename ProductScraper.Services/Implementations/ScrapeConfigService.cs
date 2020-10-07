91b14aad0939100b1184119a44212f5b6d045b1c
Stevan Kostoski <stevan.kostoski@3shape.com>
2020-10-07 09:30:42 +0200

    Bug fix

--- ProductScraper.Services/Implementations/ScrapeConfigService.cs
+++ ProductScraper.Services/Implementations/ScrapeConfigService.cs
@@ -28,31 +28,31 @@ namespace ProductScraper.Services.Implementations
             scrapeConfig.RowKey = scrapeConfig.Id.ToString();
             scrapeConfig.PartitionKey = scrapeConfig.URL.ToCoreUrl();
 
-            string url = _settings.Value.AzureFunctionURL + FunctionName.AddScrapeConfig + "/" + _settings.Value.AzureFunctionCode;
+            string url = _settings.Value.AzureFunctionURL + FunctionName.AddScrapeConfig + "?code=" + _settings.Value.AzureFunctionCode;
             await _httpHandlerService.HandlePostRequest(url, scrapeConfig);
         }
 
         public async Task DeleteAsync(string partitionKey, string rowKey)
         {
-            string url = _settings.Value.AzureFunctionURL + FunctionName.DeleteScrapeConfig + $"/{partitionKey}/{rowKey}/" + _settings.Value.AzureFunctionCode;
+            string url = _settings.Value.AzureFunctionURL + FunctionName.DeleteScrapeConfig + $"/{partitionKey}/{rowKey}?code=" + _settings.Value.AzureFunctionCode;
             await _httpHandlerService.HandlePostRequest(url, null);
         }
 
         public async Task<IList<ScrapeConfig>> GetAllAsync()
         {
-            string url = _settings.Value.AzureFunctionURL + FunctionName.GetAllScrapeConfigs + "/" + _settings.Value.AzureFunctionCode;
+            string url = _settings.Value.AzureFunctionURL + FunctionName.GetAllScrapeConfigs + "?code=" + _settings.Value.AzureFunctionCode;
             return await _httpHandlerService.HandleGetRequest<IList<ScrapeConfig>>(url);
         }
 
         public async Task<ScrapeConfig> GetDetailsAsync(string partitionKey, string rowKey)
         {
-            string url = _settings.Value.AzureFunctionURL + FunctionName.GetScrapeConfig + $"/{partitionKey}/{rowKey}/" + _settings.Value.AzureFunctionCode;
+            string url = _settings.Value.AzureFunctionURL + FunctionName.GetScrapeConfig + $"/{partitionKey}/{rowKey}?code=" + _settings.Value.AzureFunctionCode;
             return await _httpHandlerService.HandleGetRequest<ScrapeConfig>(url);
         }
 
         public async Task UpdateAsync(ScrapeConfig scrapeConfig)
         {
-            string url = _settings.Value.AzureFunctionURL + FunctionName.UpdateScrapeConfig + "/" + _settings.Value.AzureFunctionCode;
+            string url = _settings.Value.AzureFunctionURL + FunctionName.UpdateScrapeConfig + "?code=" + _settings.Value.AzureFunctionCode;
             await _httpHandlerService.HandlePostRequest(url, scrapeConfig);
         }
     }
